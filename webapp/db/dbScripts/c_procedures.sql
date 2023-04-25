3651dfd1f6ae722285c4bd9dfa31570df9383ffc
Alexandre Doneux <he201962@students.ephec.be>
2023-04-26 00:48:29 +0200

    [fix] Fixing InsertData procedure
    Related to US #11.
    
    Forgot to declare monitoringId variable before setting it's value.

--- webapp/db/dbScripts/c_procedures.sql
+++ webapp/db/dbScripts/c_procedures.sql
@@ -3,12 +3,13 @@ DELIMITER //
 CREATE PROCEDURE InsertData(IN TempValue FLOAT, IN FloatDensityValue FLOAT, IN RefractDensityValue Float,
                             IN Timestamp DATETIME, IN ProbeId INT)
 BEGIN
+    DECLARE monitoringId INT;
     SET monitoringId = (SELECT MonitorId
                         FROM Monitoring
                         WHERE ProbeId=ProbeId);
     
-    INSERT INTO Data (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitorId)
-        TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitorId;
+    INSERT INTO Data (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitorId) Values
+        (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitoringId);
 
 END//
 
