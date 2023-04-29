4957674f401c3fb06bf7a6308cf0e43d6d3348f1
Alexandre Doneux <he201962@students.ephec.be>
2023-04-29 23:44:16 +0200

    [fix] Fixing problem with multiple row subquery
    US #11
    
    The SELECT subquery kept returning multiple rows and it looked liked the WHERE statement was not considered. The problem
    was with the variable names. Same parameter name as row names caused problems.

--- webapp/db/dbScripts/c_procedures.sql
+++ webapp/db/dbScripts/c_procedures.sql
@@ -1,16 +1,21 @@
 DELIMITER //
 
-CREATE PROCEDURE InsertData(IN TempValue FLOAT, IN FloatDensityValue FLOAT, IN RefractDensityValue Float,
-                            IN Timestamp DATETIME, IN ProbeId INT)
+CREATE PROCEDURE InsertData(IN _tempValue FLOAT, IN _floatDensityValue FLOAT, IN _refractDensityValue Float,
+                            IN _timestamp DATETIME, IN _probeId INT)
+
 BEGIN
     DECLARE monitoringId INT;
     SET monitoringId = (SELECT MonitorId
                         FROM Monitoring
-                        WHERE ProbeId=ProbeId);
-    
-    INSERT INTO Data (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitorId) Values
-        (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitoringId);
+                        WHERE ProbeId=_probeId);
+
+    SELECT monitoringId;
+
+    INSERT INTO Data (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitorId) VALUES
+      (_tempValue, _floatDensityValue, _refractDensityValue, _timestamp, monitoringId);
+
 
 END//
+
 
 DELIMITER ;
