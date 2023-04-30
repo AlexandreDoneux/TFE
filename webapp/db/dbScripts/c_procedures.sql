f2cb1405d871c67fcdb077ddc78e1899c76b8fc6
Alexandre Doneux <he201962@students.ephec.be>
2023-04-30 17:19:26 +0200

    [feat] Adding error management in InsertData SQL procedure.
    US #11
    
    If they were too many active monitoring for a probe, or none, nothing wzas done. Now sending response depending of the
    number of active monitorings for a probe (none, one, multiple).

--- webapp/db/dbScripts/c_procedures.sql
+++ webapp/db/dbScripts/c_procedures.sql
@@ -1,7 +1,7 @@
 DELIMITER //
 
 CREATE PROCEDURE InsertData(IN _tempValue FLOAT, IN _floatDensityValue FLOAT, IN _refractDensityValue Float,
-                            IN _timestamp DATETIME, IN _probeId INT)
+                            IN _timestamp DATETIME, IN _probeId INT, OUT _response VARCHAR(40))
 
 BEGIN
     DECLARE monitoringId INT;
@@ -9,13 +9,23 @@ BEGIN
                         FROM Monitoring
                         WHERE ProbeId=_probeId);
 
-    SELECT monitoringId;
+    IF monitoringId = NULL THEN
+      SET _response = "no active monitoring available";
 
-    INSERT INTO Data (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitorId) VALUES
-      (_tempValue, _floatDensityValue, _refractDensityValue, _timestamp, monitoringId);
+    ELSEIF monitoringID = 1 THEN
+      INSERT INTO Data (TempValue, FloatDensityValue, RefractDensityValue, Timestamp, MonitorId) VALUES
+        (_tempValue, _floatDensityValue, _refractDensityValue, _timestamp, monitoringId);
+    
+      SET _response = "ok";
+      
+    ELSE
+      SET _response = "too many";
+
+    END IF;
 
 
 END//
 
+-- call insertData(1.1,3.3,2.2,'2023-04-28 14:30:00',1);
 
 DELIMITER ;
