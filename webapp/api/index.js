9d4620993daa55359459d8907b784e84ee00f23a
Alexandre Doneux <he201962@students.ephec.be>
2023-05-01 12:27:10 +0200

    [feat] Adding connexion from API to DB. Creating /send_data API endpoint.
    US #11
    
    Creating /send_data endpoint used to store a new data entry inside the Data table in the database. Currently using a
    hardcoded date to store in the data entry.
    
    Notable problem encountered : When connecting to the mariadb container from the api container I used host "localhost".
    The api container was looking for a database on itself and not the localhost (where the containers run.).

--- webapp/api/index.js
+++ webapp/api/index.js
@@ -1,9 +1,52 @@
 const express = require('express')
 const app = express()
 
+// using pool defined in db.js
+const pool = require('./db');
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+// temporary endpoint to see the effect of /send_data
+app.get('/data', async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    //const response = await conn.query("CALL insertData(1.1,3.3,2.2,'2023-04-28 14:30:00',1);");
+    const response = await conn.query("SELECT * FROM Monitoring;");
+    res.send(response);
+  } catch (error) {
+    throw error;
+  } finally {
+    if (conn) conn.release(); // release connection back to pool
+  }
+});
+
+
+app.post('/send_data', async (req, res) => {
+  const { send_timestamp, probe_id, data_timestamp, temperature, float_density, refract_density } = req.body;
+  let conn;
+
+  try{
+    conn = await pool.getConnection();
+    // data_timestamp to SQL DATETIME
+
+    //let query = `CALL insertData(${temperature},${float_density},${refract_density},${date},${probe_id});`
+    let query = `CALL insertData(${temperature},${float_density},${refract_density},'2023-04-28 14:30:00',${probe_id});`
+    const response = await conn.query(query);
+    res.send(response)
+    res.status(201).json({ message: 'Data successfully saved' });
+
+  }catch(error){
+    print(error)
+    // error catch
+  }
+  
+});
+
 
 app.listen(3001, () => {
   console.log('Server started on port 3001')
