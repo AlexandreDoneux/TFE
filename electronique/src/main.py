f86670d63fa5410927bc8a3efc5be61d16c3307f
Alexandre Doneux <he201962@students.ephec.be>
2023-05-04 23:36:54 +0200

    [feat] Adding Temperature reading function using the TMP36 temperature sensor
    US #3
    
    Adding a temperature reading function reading the voltage of an adc pin of the raspberry. Transforming the read value
    into miliVolts and then into degrees Celsius.

--- electronique/src/main.py
+++ electronique/src/main.py
@@ -1,12 +1,47 @@
-# A mettre dans un fichier python à part (secrets.py / secrets.json)
+# A mettre dans un fichier python à part (secrets.py)
 # SSID de point d'accès wifi
 SSID = "SSID"
 # Mot de passe wifi
 PASSWORD = "PASSWORD"
 
-import network
+# number of minutes between each measure and sending of the data to distant database
+#data_interval = 30
+data_interval = 0.1 # test value (each 6 seconds)
+
+
+#---------- imports ----------------
+
+from machine import ADC, Pin
 import time
+import network
+#import secrets
 import urequests
+
+
+#---------- pin variables --------------
+
+adc_pin = ADC(Pin(26)) #ADC pin for temperature measure
+
+
+
+
+
+
+# ----------- functions -----------------
+
+def measure_temp():
+    """
+    Measures temperature in degress Celsius 
+    """
+    value = adc_pin.read_u16()
+    mv = 3300.0 * value / 65535
+    temp = (mv-500)/10
+    
+    return temp
+
+
+# ----------- wifi connection ------------
+
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -14,7 +49,9 @@ wlan.connect(SSID,PASSWORD)
 
 print(wlan.isconnected())
 print(wlan.ifconfig())
-   
+
+
 # Remplacer par appel vers API personelle
 astronauts = urequests.get("http://api.open-notify.org/astros.json").json()
 print(astronauts)
+
