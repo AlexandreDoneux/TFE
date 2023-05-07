b7b7cc3ad0488d1dfbac7368ec18e297946e620b
Alexandre Doneux <he201962@students.ephec.be>
2023-05-07 12:56:14 +0200

    [feat] Adding transformDate function to "align" dates in the same timeframe
    US #11
    
    When powering back on the raspberry it's internal clock will restart at a certain date (1 Jan. 2021, 1 Jan. 2015, 1 Jan.
    1970, ... -> need to check). The problem will be to knwo exactly when the measures were taken.
    
    To do that we send the date of the measure and the date at which the data is sent (will be usefull later when sending
    multiple measured data). On the api's side we will retrieve the date the request in sent and modify the measure date
    compared to the actal date. This way we will have the correct dates at which the data was measured.

--- webapp/api/index.js
+++ webapp/api/index.js
@@ -23,6 +23,25 @@ function createNewObject(okPacket) {
 }
 
 
+function transformDate(date_to_transform){
+  /**
+   * Receives a date as a tuple and creates a new date tuple of the first date based on the current date retrieved by the system. The idea is to 
+   * reposition the date received inside the system's date "frame".
+   */
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1; // Month starts from 0, add 1 to get the correct month number
+  const day = currentDate.getDate();
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const second = currentDate.getSeconds();
+  const currentDateTuple = [year, month, day, hour, minute, second];
+
+  
+}
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -44,6 +63,12 @@ app.get('/data', async (req, res) => {
 });
 
 
+app.get('/current-date', (req, res) => {
+  const currentDate = new Date();
+  res.send(currentDate);
+});
+
+
 app.post('/send_data', async (req, res) => {
   console.log(req.body)
   const { send_timestamp, probe_id, data_timestamp, temperature, float_density, refract_density } = req.body;
@@ -52,6 +77,7 @@ app.post('/send_data', async (req, res) => {
   try{
     conn = await pool.getConnection();
     // data_timestamp to SQL DATETIME
+    
 
     //let query = `CALL insertData(${temperature},${float_density},${refract_density},${date},${probe_id});`
     let query = `CALL insertData(${temperature},${float_density},${refract_density},'2023-04-28 14:30:00',${probe_id}, @_response);`
