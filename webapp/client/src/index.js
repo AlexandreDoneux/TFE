30a3fe23a5aa3041fc7494a5d326838a6db82450
Alexandre Doneux <he201962@students.ephec.be>
2023-05-08 21:56:06 +0200

    [feat] Adding Chart React Component
    US #18
    
    Adding Chart static component that displays Default data.

--- webapp/client/src/index.js
+++ webapp/client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Chart from './components/chart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Chart/>
+    <span>
+      test
+    </span>
   </React.StrictMode>
 );
 
