726d26a823ac6b2e5e540e7cc50733fdedcf0d30
Alexandre Doneux <he201962@students.ephec.be>
2023-05-23 22:30:26 +0200

    [feat] Adding /connexion, /connexion2 and /show_cookie endpoints to test cookie setup
    US #2
    
    In order to test the setup of cookies for user connection check we added /connexion that setups an user_id cookie, /connexion2 sets up
    a dummy cookie and /show_cookie sends back the cookies stored on the web page.

--- webapp/api/routes/account.js
+++ webapp/api/routes/account.js
@@ -8,9 +8,47 @@ const pool = require('../db');
 // Endpoint to set the cookie
 router.get('/connexion', (req, res) => {
     const userId = 1
-    res.cookie('user_id', userId, { maxAge: 3600000 });
-    res.send('Cookie has been set');
+    //res.cookie('user_id', userId, { maxAge: 3600000 });
+    //res.send('Cookie has been set');
+
+    return res.status(200).cookie("user_id", userId, {
+                        
+        //secure: true, // -> https
+        httpOnly : true,
+        sameSite : "none", //Should be "strict" in prod
+        maxAge : 1 * 60 * 60 * 2 * 1000, //2 hours
+        //signed: true
+    }).send("Cookie has been set")
 });
+
+router.get('/connexion2', (req, res) => {
+    const dummy = 99
+    //res.cookie('user_id', userId, { maxAge: 3600000 });
+    //res.send('Cookie has been set');
+
+    return res.status(200).cookie("dummy", dummy, {
+                        
+        //secure: true, // -> https
+        httpOnly : true,
+        sameSite : "none", //Should be "strict" in prod
+        maxAge : 1 * 60 * 60 * 2 * 1000, //2 hours
+        signed: true
+    }).send("Cookie has been set")
+});
+
+
+router.get('/show_cookie', (req, res) => {
+    const cookies = req.headers.cookie; // string contenant tous les cookies séparés par ;
+    const signed_cookies = req.signedCookies; // [Object: null prototype] { dummy: '99' }
+    //const user_id = cookie.user_id;
+
+
+    console.log(cookies)
+    console.log(signed_cookies)
+    //console.log(user_id)
+    res.send(`Cookie has `);
+    //res.send(`Cookie has user_id : ${user_id}`);
+})
 
 
 module.exports = router;
