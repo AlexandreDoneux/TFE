6edba13a608e76330e2428e0b5f40452f860b22c
Alexandre Doneux <he201962@students.ephec.be>
2023-06-02 17:21:12 +0200

    [refactor] Changing tables and insert script to delete salt column in User and Probe tables and insert a hashed password using Argon2
    Originally we wanted to use a column for the password and a column for the password and a column for the salt inside User and
    Probe tables. However it is easier to store as one string when using Argon2 for backend hashing. When hashing a passwor Argon2
    generates a string with multipe parameters and the hashed password. To compare a given password to the stored hash Argon2 will
    recalculate the salt using the parameters of the stored string in DB. It will then rehash the password and compare the two hashes.

--- webapp/db/dbScripts/b_insert.sql
+++ webapp/db/dbScripts/b_insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO User (Name, Firstname, Email, Password, Salt)
+INSERT INTO User (Name, Firstname, Email, Password)
 VALUES
-('Doe', 'John', 'john@doe.com', 'passjohn', NULL),
-('Lang', 'Jack', 'jack@lang.com', 'passjack', NULL);
+('Doe', 'John', 'john@doe.com', 'passjohn'),
+('Lang', 'Jack', 'jack@lang.com', 'passjack');
 
-INSERT INTO Probe (Name, Password, Salt, UserId)
+INSERT INTO Probe (Name, Password, UserId)
 VALUES
-('Toto', 'mdptoto', NULL, 1),
-('Tata', 'mdptata', NULL, 1),
-('Tutu', 'mdptutu', NULL, 2);
+('Toto', '$argon2id$v=19$m=65536,t=3,p=4$HrovBOZFKoC1Evst1LagPw$PTnKwETU1QX1xLUSahLtsPNEQI0em0VxbMnCIRQcMaw', 1), -- password : mdptoto
+('Tata', '$argon2id$v=19$m=65536,t=3,p=4$ml/aLKFjJbNAC2EOrfvfBw$MJa7JftJ0I1a6R1xIMqL2cl8Cv0W0gPAPKByJ5U1LOI', 1), -- password : mdptata
+('Tutu', '$argon2id$v=19$m=65536,t=3,p=4$XJKMqOIuSRQjPBkQ/O7GGA$zJnNMrKGcqnwztVGC9eHxHi8+fkxUjyukKbNUF56ilc', 2); -- password : mdptutu
 
 
 INSERT INTO Monitoring (StartDate, EndDate, ProbeId)
