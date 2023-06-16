e9ffb96245001a298030f6f6cac33b8e3fa71585
Alexandre Doneux <he201962@students.ephec.be>
2023-06-16 22:43:23 +0200

    [feat] Checking request body for /data/send_data endpoint
    US #22
    
    Wasn't checking the values given as the body request for /data/send_data endpoint.
    
    Now checking if al the parameters are of the riht type and structure.

--- webapp/api/routes/data.js
+++ webapp/api/routes/data.js
@@ -48,9 +48,30 @@ router.post('/send_data', async (req, res) => {
   const { send_timestamp, probe_id, probe_password, ...data } = req.body;
   let conn;
   let responses = [];
-  
 
   try {
+    // Validate request body structure
+    if (!Array.isArray(send_timestamp) || send_timestamp.length !== 6) {
+      return res.status(400).send('Invalid send_timestamp');
+    }
+    if (!Number.isInteger(probe_id) || probe_id <= 0) {
+      return res.status(400).send('Invalid probe_id');
+    }
+    if (typeof probe_password !== 'string') {
+      return res.status(400).send('Invalid probe_password');
+    }
+    for (const key in data) {
+      const item = data[key];
+      if (
+        typeof item !== 'object' ||
+        typeof item.temperature !== 'number' ||
+        typeof item.float_density !== 'number' ||
+        typeof item.refract_density !== 'number'
+      ) {
+        return res.status(400).send('Invalid data');
+      }
+    }
+
     conn = await pool.getConnection();
 
     let query = `CALL RetrieveProbePassword(${probe_id});`;
@@ -58,33 +79,36 @@ router.post('/send_data', async (req, res) => {
 
     probe_connect[1] = createNewObject(probe_connect[1]);
 
-    password_correct = await checkPasswordArgon2(probe_connect[0][0]["Response"], probe_password);
+    const password_correct = await checkPasswordArgon2(
+      probe_connect[0][0]['Response'],
+      probe_password
+    );
 
-    if(password_correct){
+    if (password_correct) {
       for (const [timestamp, item] of Object.entries(data)) {
         const { temperature, float_density, refract_density } = item;
-  
+
         // data_timestamp to SQL DATETIME + transpose to real time "frame"
-        const new_data_timestamp = await transformDate(send_timestamp, timestamp.split(" "));
+        const new_data_timestamp = await transformDate(
+          send_timestamp,
+          timestamp.split(' ')
+        );
         const inter_date = new Date(Date.UTC(...new_data_timestamp)).toISOString();
-        const date = inter_date.replace("T", " ").replace("Z", ""); // "2023-06-07T16:19:38.000Z" -> T and Z must go away
-  
+        const date = inter_date.replace('T', ' ').replace('Z', ''); // "2023-06-07T16:19:38.000Z" -> T and Z must go away
+
         let query = `CALL insertData(${temperature},${float_density},${refract_density},"${date}",${probe_id});`;
         let response = await conn.query(query);
-  
+
         response[1] = createNewObject(response[1]);
         response[0] = response[0][0];
-  
+
         responses.push(response);
       }
-  
-      res.send(responses);
-    }
-    else{
-      res.send("wrong password");
-    }
 
-    
+      res.send(responses);
+    } else {
+      res.send('Wrong password');
+    }
   } catch (error) {
     console.log(error);
     // error catch
