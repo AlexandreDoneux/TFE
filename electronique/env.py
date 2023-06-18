f10c9eb9377c6969adba08ef4dcc701b5edb08d7
Alexandre Doneux <he201962@students.ephec.be>
2023-06-18 11:57:10 +0200

    [feat] Adding api_ip_address inside env.py file for the probe

--- electronique/env.py
+++ electronique/env.py
@@ -4,7 +4,12 @@ SSID = "SSID"
 PASSWORD = "PASSWORD"
 
 # number of minutes between each measure and sending of the data to distant database
-data_interval = 30
+data_interval = 0.05
+
+
+# DEV, API ip address
+api_ip_address = "IP_ADDRESS_OR_DOMAIN_NAME"
+
 
 # probe_id and password
 probe_id = 1
