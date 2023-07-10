c2f4924586530ff071cb4eab18be41ab29c74eee
Alexandre Doneux <he201962@students.ephec.be>
2023-07-10 23:06:59 +0200

    [refactor] Changing MPU library and code for GY-521
    The previous library and code did only show the data and did not calculate the pitch and roll of the GY-521.
    
    Using another library and code to access these (https://how2electronics.com/interfacing-mpu6050-with-raspberry-pi-pico-micropython/).

--- electronique/src/gy521_pitch.py
+++ electronique/src/gy521_pitch.py
@@ -1,17 +1,57 @@
-from imu import MPU6050
-import time
 from machine import Pin, I2C
+import utime
+import math
+from mpu6050 import init_mpu6050, get_mpu6050_data
 
-i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
-imu = MPU6050(i2c)
+i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=400000)
+init_mpu6050(i2c)
 
+
+
+def calculate_tilt_angles(accel_data):
+    x, y, z = accel_data['x'], accel_data['y'], accel_data['z']
+ 
+    tilt_x = math.atan2(y, math.sqrt(x * x + z * z)) * 180 / math.pi
+    tilt_y = math.atan2(-x, math.sqrt(y * y + z * z)) * 180 / math.pi
+    tilt_z = math.atan2(z, math.sqrt(x * x + y * y)) * 180 / math.pi
+ 
+    return tilt_x, tilt_y, tilt_z
+ 
+def complementary_filter(pitch, roll, gyro_data, dt, alpha=0.98):
+    pitch += gyro_data['x'] * dt
+    roll -= gyro_data['y'] * dt
+ 
+    pitch = alpha * pitch + (1 - alpha) * math.atan2(gyro_data['y'], math.sqrt(gyro_data['x'] * gyro_data['x'] + gyro_data['z'] * gyro_data['z'])) * 180 / math.pi
+    roll = alpha * roll + (1 - alpha) * math.atan2(-gyro_data['x'], math.sqrt(gyro_data['y'] * gyro_data['y'] + gyro_data['z'] * gyro_data['z'])) * 180 / math.pi
+
+    
+    return pitch, roll
+
+    
+ 
+pitch = 0
+roll = 0
+prev_time = utime.ticks_ms()
+ 
 while True:
-    ax=round(imu.accel.x,2)
-    ay=round(imu.accel.y,2)
-    az=round(imu.accel.z,2)
-    gx=round(imu.gyro.x)
-    gy=round(imu.gyro.y)
-    gz=round(imu.gyro.z)
-    tem=round(imu.temperature,2)
-    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
-    time.sleep(0.2)
+    data = get_mpu6050_data(i2c)
+    curr_time = utime.ticks_ms()
+    dt = (curr_time - prev_time) / 1000
+ 
+    tilt_x, tilt_y, tilt_z = calculate_tilt_angles(data['accel'])
+    pitch, roll = complementary_filter(pitch, roll, data['gyro'], dt)
+ 
+    prev_time = curr_time
+    
+ 
+    print("Temperature: {:.2f} °C".format(data['temp']))
+    print("Tilt angles: X: {:.2f}, Y: {:.2f}, Z: {:.2f} degrees".format(tilt_x, tilt_y, tilt_z))
+    print("Pitch: {:.2f}, Roll: {:.2f} degrees".format(pitch, roll))
+
+    print("Acceleration: X: {:.2f}, Y: {:.2f}, Z: {:.2f} g".format(data['accel']['x'], data['accel']['y'], data['accel']['z']))
+    print("Gyroscope: X: {:.2f}, Y: {:.2f}, Z: {:.2f} °/s".format(data['gyro']['x'], data['gyro']['y'], data['gyro']['z']))
+    
+	# Delay for 1 seconds
+    utime.sleep(1)
+
+
