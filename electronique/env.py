70442a552cfa929c7752ff0d55c9f910bf9d9b67
Alexandre Doneux <he201962@students.ephec.be>
2023-07-11 22:28:57 +0200

    [feat] Calling get_pitch_and_roll function into main.py. Adding function parameters and tilting parametering mode
    To estimate the density using the pitch of the probe we need to use a polynomial function (2nd degree). To find the parameters of that function we
    need to test the probe with multiple samples. Using a parametering mode will allow us to call an endpoint only displaying the pitch. Using it we will
    find the function.
    
    Adding the function parameters, calling the pitch and roll function into man.py and adding the probe password (forgot to add it before).
     # Please enter the commit message for your changes. Lines starting

--- electronique/env.py
+++ electronique/env.py
@@ -8,3 +8,14 @@ data_interval = 30
 
 # probe_id and password
 probe_id = 1
+probe_password = "PROBE_PASSWORD"
+
+# -------------- tilt density parameters
+
+# used for testing tilt and density to interpolate a function (2nd degree) to estimate density : True -> testing mode active
+tilting_parametring_mode = False
+
+# estimation function parameters : a.x^2+b.x+c
+func_param_a = 1
+func_param_b = 1
+func_param_c = 1
