e6e14e1ecdc1a7776f5cd523829135c357e83e39
Alexandre Doneux <he201962@students.ephec.be>
2023-07-24 13:18:54 +0200

    [refactor] Deleting old API endpoints used for dev purposes

--- webapp/api/routes/data.js
+++ webapp/api/routes/data.js
@@ -6,41 +6,6 @@ const pool = require('../db');
 const argon2 = require('argon2');
 
 const { createNewObject, transformDate, checkPasswordArgon2, hashPasswordWithArgon2 } = require("../api_functions.js");
-  
-  
-// temporary endpoint to see the effect of /send_data
-router.get('/', async (req, res) => {
-    let conn;
-    const cookies = req.signedCookies;
-
-    if(cookies.session_id){
-        session_id = cookies.session_id;
-
-        try {
-            conn = await pool.getConnection();
-            let connected = await conn.query(`CALL CheckSessionExists(${session_id})`);
-            connected[1] = createNewObject(connected[1])
-  
-            if(connected[0][0]["Response"]){
-              let response = await conn.query("SELECT * FROM Session;");
-              res.send(response);
-            }else{
-              res.status(400).send("not connected (session)");
-            }
-            
-          } catch (error) {
-            throw error;
-          } finally {
-            if (conn) conn.release(); // release connection back to pool
-          }
-
-    }else{
-        res.status(400).send("not connected (cookie)")
-    }
-    
-});
-  
-
 
 
 router.post('/send_data', async (req, res) => {
@@ -201,31 +166,6 @@ router.post('/get_monitoring', async (req, res) => {
     if (conn) conn.release(); // release connection back to pool
   }
 });
-
-
-/*
-router.post('/monitoring_data', async (req, res) => {
-  console.log(req.body) 
-
-  const { monitor_id } = req.body;
-
-  let conn;
-  try {
-    conn = await pool.getConnection();
-    let response = await conn.query(`CALL SelectData(${monitor_id});`);
-
-    response[1] = createNewObject(response[1])
-    
-    console.log(response)
-    res.send(response);
-  } catch (error) {
-    throw error;
-    //res.send("error") //send error and not throw error -> later
-  } finally {
-    if (conn) conn.release(); // release connection back to pool
-  }
-});
-*/
 
 
 
