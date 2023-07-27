37c08049b561ff2770f9ec38688ff2e82d6c1282
Alexandre Doneux <he201962@students.ephec.be>
2023-07-28 00:10:32 +0200

    [fix] correct handling of errors with probe delete
    Need to see error messages back and work on it because they are not working properly for the moment (not usefull in the way they are implemented).

--- webapp/client/src/components/ProbeDeletionPopup.js
+++ webapp/client/src/components/ProbeDeletionPopup.js
@@ -5,9 +5,11 @@ import { Button, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/
 const ProbeDeletionPopup = (props) => {
 
   const {showPopup, onClose,  probe_id} = props;
+  const [errorMessage, setErrorMessage] = useState('');
 
   const DeleteProbe = async(probe_id)=>{
-    const result = await axios.post(`http://localhost:3001/probe/delete`, 
+    try{
+      const result = await axios.post(`http://localhost:3001/probe/delete`, 
       {
         "probe_id" : probe_id,
       },
@@ -16,6 +18,31 @@ const ProbeDeletionPopup = (props) => {
       }
     );
     window.location = await `/`;
+    }
+    catch(error){ 
+        console.log(error)
+        if (error.response && error.response.status === 400) { // if code error 400
+            const errorMessage = error.response.data;
+            if (errorMessage === "Probe does not belong to the user") {
+                // should be checked before beeing able to access the form
+                setErrorMessage("Probe does not belong to the user");
+            } 
+            else if (errorMessage === "Active monitoring on the probe. Cannot delete probe") {
+                setErrorMessage("Active monitoring on the probe. Cannot delete probe");
+            }
+            else if ((errorMessage === "not connected (session)") || (errorMessage === "not connected (cookie)")) {
+                setErrorMessage("You are not connected !");
+                console.log("You are not connected !");
+                await alert("You are not connected !");
+                window.location = await `/connect`;
+            }
+        }
+        else {
+            console.error('Error:', error);
+            setErrorMessage('An error occured. Contact the website administrator.');
+        }
+    }
+    
   }
 
   const handleArchiveClick = (probe_id) => {
@@ -39,6 +66,7 @@ const ProbeDeletionPopup = (props) => {
             Cancel
           </Button>
         </DialogActions>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
       </Dialog>
     </div>
   );
