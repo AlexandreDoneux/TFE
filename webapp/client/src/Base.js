9f8c7c1214f410f41fb9f3e91f1848e9ad5b64cf
Alexandre Doneux <he201962@students.ephec.be>
2023-08-02 17:23:29 +0200

    [fix] Fixing double import

--- webapp/client/src/Base.js
+++ webapp/client/src/Base.js
@@ -1,6 +1,4 @@
-
 import React, { useContext } from 'react';
-import { styled } from '@mui/system';
 import { Box, AppBar } from '@mui/material';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { unstable_styleFunctionSx, styled } from '@mui/system';
