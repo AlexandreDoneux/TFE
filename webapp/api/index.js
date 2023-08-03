fa3c79bc56d243b32e762c334a455b569ec23e8b
Alexandre Doneux <he201962@students.ephec.be>
2023-08-03 18:04:46 +0200

    [fix] Changement emplacement fichiers certificat ssl

--- webapp/api/index.js
+++ webapp/api/index.js
@@ -8,9 +8,9 @@ const fs = require('fs');
 const https = require('https');
 
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.alexandre.doneux.eu/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/www.alexandre.doneux.eu/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/www.alexandre.doneux.eu/chain.pem', 'utf8');
+const privateKey = fs.readFileSync('/www.alexandre.doneux.eu/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/www.alexandre.doneux.eu/cert.pem', 'utf8');
+const ca = fs.readFileSync('/www.alexandre.doneux.eu/chain.pem', 'utf8');
 
 const credentials = {
 	key: privateKey,
