218853a73dfb8404bfa4f7bb172a4c95042d1170
Alexandre Doneux <he201962@students.ephec.be>
2023-08-06 12:52:38 +0200

    [fix] Trying some other things for WAF

--- webapp/client/WAF.sh
+++ webapp/client/WAF.sh
@@ -4,8 +4,8 @@
 
 apt-get install g++ apt-utils autoconf automake build-essential libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev -y
 
-# need "cd /opt/"
 
+cd /opt/
 git clone https://github.com/SpiderLabs/ModSecurity
 cd ModSecurity/
 git submodule init
@@ -23,9 +23,15 @@ git clone https://github.com/SpiderLabs/ModSecurity-nginx
 wget http://nginx.org/download/nginx-1.18.0.tar.gz # why this ?
 tar -xvzf nginx-1.18.0.tar.gz # why this ?
 cd /opt/nginx-1.18.0
-./configure --add-dynamic-module=/opt/ModSecurity-nginx --with-compat
-make
-make install
+./configure --add-dynamic-module=/opt/ModSecurity-nginx --with-compat --with-openssl=/usr/include/openssl/ # with-openssl -> opsshield tutorial
+#make
+make modules # this instead of make ?
+make install  # not present in opsshield tutorial
+cp objs/ngx_http_modsecurity_module.so /usr/share/nginx/modules/ # copy to modules directory -> why ? Will NGINX find it automatically ? Does is replace the load_module in nginx.conf ?
+#echo "load_module modules/ngx_http_modsecurity_module.so;" > /etc/nginx/modules-enabled/50-mod-http-modsecurity.conf 
+
+
+# /usr/lib/nginx/modules/ -> in nginx -V => modules-path
 
 
 # OWASP security rule set -> download in source code and use volume for containers ????, simply have the .conf file ???
