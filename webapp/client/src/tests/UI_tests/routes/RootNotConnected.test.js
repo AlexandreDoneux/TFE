06be85b75e71af268331b61b6ec6e7de01728fcc
Alexandre Doneux <he201962@students.ephec.be>
2023-08-28 17:49:33 +0200

    [test] Adding routing tests for "/" route

---
+++ webapp/client/src/tests/UI_tests/routes/RootNotConnected.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import {beforeEach, afterEach} from "../testing_functions";
+import axios from 'axios'; 
+import { UserProvider, UserContext } from '../../../UserContext';
+import Base from "../../../Base";
+
+
+
+// Mock UserContext values
+// jest.fn() can be checked on how many times it has been called.
+const contextValue = {
+  isConnected: false,
+  setIsConnected: jest.fn(),
+  removeIsConnected: jest.fn(),
+};
+
+
+// Mock axios module
+jest.mock('axios');
+
+describe('/ route not connected', () => {
+  it('checks the MenuDrawer component is not rendered and the connect button is.', async () => {
+
+
+    // Mock axios.post for fetching archived monitoring IDs and data
+    axios.post
+      .mockResolvedValueOnce({ data: [
+        {
+            "Response": "user exists",
+            "ProbeIds": "1",
+            "ProbeNames": "Toto",
+            "ActiveMonitoringId": 1
+        },
+        {
+            "Response": "user exists",
+            "ProbeIds": "2",
+            "ProbeNames": "Tata",
+            "ActiveMonitoringId": 0
+        }
+    ] })  // mock for /probe/get_active endpoint of MenuDrawer
+      
+
+
+    const forced_route = "/";
+
+    render(
+      <UserContext.Provider value={contextValue}>
+        {/* Render the ArchivedList component */}
+        <Base forced_route={forced_route}></Base>
+      </UserContext.Provider>,
+    );
+    
+
+    // Wait for data to load
+    await waitFor(() => {
+      // Check the MenuDrawer component is present. To ceck for the inside of the drawer we need to click on it. We will do that in another test.
+      expect(screen.queryByText('Connect')).toBeInTheDocument();
+      expect(screen.queryByText('Menu')).not.toBeInTheDocument();
+
+    });
+  });
+
+});
