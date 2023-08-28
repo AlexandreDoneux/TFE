d47647350ddd86504827a25b30f88880a35717c4
Alexandre Doneux <he201962@students.ephec.be>
2023-08-28 22:25:32 +0200

    [test] Adding ResizeObserver mock inside beforeEach() and afterEach()
    Using ResponsiveContainer inside Chart component. It uses ResizeObserver that need to be mocked for tests.
    
    Also adding restoring all mocks in afterEach() and deleting the ResizeObserver mock in beforeEach().

--- webapp/client/src/tests/UI_tests/testing_functions.js
+++ webapp/client/src/tests/UI_tests/testing_functions.js
@@ -1,7 +1,16 @@
 import { unmountComponentAtNode } from "react-dom";
+const { ResizeObserver } = window;
 
 let container = null;
 beforeEach(() => {
+  //mocking ResizeObserver (used by ResponsiveContainer from recharts )
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+  
   // setup a DOM element as a render target
   container = document.createElement("div");
   document.body.appendChild(container);
@@ -9,6 +18,10 @@ beforeEach(() => {
 
 afterEach(() => {
   // cleanup on exiting
+  window.ResizeObserver = ResizeObserver;
+
+  jest.restoreAllMocks();
+
   unmountComponentAtNode(container);
   container.remove();
   container = null;
