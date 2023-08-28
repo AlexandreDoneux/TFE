b0e441c6f09825d25a01038ed4fd0ecff3cefde8
Alexandre Doneux <he201962@students.ephec.be>
2023-08-28 16:46:39 +0200

    [test] refactor by replacing .getByText() with .queryByText()
    I was looking for missing elements with .getByText() but using it will raise an error. .queryByText() will not so it can be used to check for
    elements/texts/components that are not supposed to be there.

--- webapp/client/src/tests/UI_tests/ArchivedList.test.js
+++ webapp/client/src/tests/UI_tests/ArchivedList.test.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-//import ArchivedList from '../../components/ArchivedList';
-import {beforeEach, afterEach} from "./testing_functions";
+import '@testing-library/jest-dom'
+import {beforeEach, afterEach} from "./testing_functions"; // testing functions not used directly in code. Still effective ?
 import axios from 'axios'; 
 import { UserProvider, UserContext } from '../../UserContext';
 import Base from "../../Base";
-
-//
-import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { useHistory } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
-
-
-import { unmountComponentAtNode } from "react-dom";
 
 
 
 
 // Mock UserContext values
+// jest.fn() can be checked on how many times it has been called.
 const contextValue = {
   isConnected: true,
   setIsConnected: jest.fn(),
@@ -41,48 +32,49 @@ jest.mock('react-router-dom', () => ({
 // Mock axios module
 jest.mock('axios');
 
-import ArchivedList from '../../components/ArchivedList';
-
 describe('ArchivedList', () => {
   it('displays connected user data', async () => {
 
-    // Mock axios.post for fetching archived monitoring IDs
-    axios.post.mockResolvedValueOnce( 
-      {data : ["3","4"]}
-    );
-    
 
-    axios.post.mockResolvedValueOnce(
-      {data :
+    // Mock axios.post for fetching archived monitoring IDs and data
+    axios.post
+      .mockResolvedValueOnce({ data: [
         {
+            "Response": "user exists",
+            "ProbeIds": "1",
+            "ProbeNames": "Toto",
+            "ActiveMonitoringId": 1
+        },
+        {
+            "Response": "user exists",
+            "ProbeIds": "2",
+            "ProbeNames": "Tata",
+            "ActiveMonitoringId": 0
+        }
+    ] })  // mock for /probe/get_active endpoint of MenuDrawer
+      .mockResolvedValueOnce({ data: ["3", "4"] }) 
+      .mockResolvedValueOnce({
+        data: {
           "monitor_id": 3,
           "name": "aaa",
           "start_date": "2023-05-28T14:30:00.000Z",
-          "end_date": null
-        }
-      }
-    );
-    axios.post.mockResolvedValueOnce(
-      {data :
-        {
+          "end_date": null,
+        },
+      })
+      .mockResolvedValueOnce({
+        data: {
           "monitor_id": 4,
           "name": "bbb",
           "start_date": "2023-05-28T14:30:00.000Z",
-          "end_date": null
-        }
-      }
-    );
+          "end_date": null,
+        },
+      });
 
     // Have to put base because could not render ArchivedList without being in a BrowserRouter tag. It is because ArchivedList uses <Link> that has to be inside a BrowserRouter or MemoryRouter.
     // Using MemoryRouter/BrowserRouter to indicate the route we are does not work because we can not have a Router inside a Router.
     //
-    // Defining a history withe createMemoryHistory with the route, modifying Base component so he can use a history value in the Router props
+    // Defining a history withe createMemoryHistory with the route, modifying Base component so he can use a history value in the Router props -> not working eather
 
-    //const history = createMemoryHistory({ initialEntries: ['/monitoring/archived'] });
-    //let history = useHistory();
-    //history.push("/monitoring/archived");
-    //const navigate = useNavigate();
-    //navigate("/session-timed-out");
 
     const forced_route = "/monitoring/archived";
 
@@ -98,9 +90,9 @@ describe('ArchivedList', () => {
     await waitFor(() => {
       // Test assertions for rendered data
       // For example, check if table cells are present
-      expect(screen.getByText('Name')).toBeInTheDocument();
-      expect(screen.getByText('Start Date')).toBeInTheDocument();
-      expect(screen.getByText('End Date')).toBeInTheDocument();
+      expect(screen.queryByText('Name')).toBeInTheDocument();
+      expect(screen.queryByText('Start Date')).toBeInTheDocument();
+      expect(screen.queryByText('End Date')).toBeInTheDocument();
     });
   });
 
